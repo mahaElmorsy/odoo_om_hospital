f1fde3e206fe8e6cce18d5dfc92e7207029127d1
mahaElmorsy <mahaosmanelmorsy@gmail.com>
2023-02-01 15:05:05 +0200

    first

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": 'Om Hospital',
+    'summary': """
+        This module is Hospital Management System """,
+    "description": 'This module is Hospital Management System',
+    "author": "Maha Osman",
+
+    "version": "1.0.0",
+    'website': "https://github.com/mahaElmorsy",
+    "data": [
+        # 'security/ir.model.access.csv',
+        # 'views/activity_views.xml',
+    ],
+    # 'depends': ['hr', 'base'],
+    "category": "",
+
+}
